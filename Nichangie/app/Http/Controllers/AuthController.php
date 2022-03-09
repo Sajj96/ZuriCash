d0625ab6457a635a29e7658cc38478ea95c075d1
Sajj96 <captainsajjad4@gmail.com>
2022-03-09 17:03:30 +0300

    Added laravel auth

--- Nichangie/app/Http/Controllers/AuthController.php
+++ Nichangie/app/Http/Controllers/AuthController.php
@@ -10,26 +10,6 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-
-    /**
-     * Where to redirect users after login.
-     *
-     * @var string
-     */
-    protected $redirectTo = RouteServiceProvider::HOME;
-
-
     public function index()
     {
         return view('auth.login');
