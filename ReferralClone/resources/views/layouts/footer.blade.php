233b3f60b18d78e0cf9fe991842d764b7d0996b4
Sajjad K Abubakari <44890673+Sajj96@users.noreply.github.com>
2022-06-02 12:42:05 +0300

    Delete ReferralClone directory

--- ReferralClone/resources/views/layouts/footer.blade.php
+++
@@ -1,8 +0,0 @@
-<footer class="main-footer">
-    <div class="footer-left">
-        <a href="templateshub.net">&copy;Copyright DELASKA AGENGY <?php echo date('Y');?></a></a>
-    </div>
-    <div class="footer-right">
-    </div>
-</footer>
-</div>
